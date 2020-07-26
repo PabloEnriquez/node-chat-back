aa67d3fc9969f8abc9cdf501c7c9c3daee730722
PabloEnriquez <12pable34@gmail.com>
2020-07-25 21:40:05 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hey Socket.io</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+http.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
