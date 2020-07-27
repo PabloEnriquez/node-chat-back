ce45eb66ab1c4eb2c380930a982186cf51db5b17
PabloEnriquez <12pable34@gmail.com>
2020-07-27 00:00:07 -0500

    lógica terminada

--- index.js
+++ index.js
@@ -8,18 +8,33 @@ app.get('/', (req, res) => {
     res.send('<h1>Hey Socket.io</h1>');
 });
 
+// const users = new Set();
+
 io.on('connection', (socket) => {
     console.log('a user connected test');
 
+    socket.on('set-username', userData => {
+        // socket.userId = userData.userId;
+        socket.userName = userData.userName;
+        // users.add(userData);
+    });
+
     socket.on('user-message', msgData => {
-        socket.broadcast.emit('user-message', msgData);
+        io.emit('user-message', msgData);
+    });
+
+    socket.on('typing', userData => {
+        socket.broadcast.emit('typing', socket.userName);
     });
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+        console.log('user disconnected');
+        // users.delete(socket.userId);
     });
 });
 
 http.listen(port, () => {
     console.log(`listening on *:${port}`);
 });
+
+module.exports = app;
